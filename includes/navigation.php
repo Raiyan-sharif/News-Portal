86e5bac8a3dd6ac9a130e65373d79fc8f145476c
Raiyan <raiyan.sharif.1234@gmail.com>
2020-12-22 02:51:57 +0600

    dinamic post

--- includes/navigation.php
+++ includes/navigation.php
@@ -1,5 +1,7 @@
+<?php include "includes/db.php"?>
 <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
         <div class="container">
+
             <!-- Brand and toggle get grouped for better mobile display -->
             <div class="navbar-header">
                 <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
@@ -8,20 +10,27 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="#">Start Bootstrap</a>
+                <a class="navbar-brand" href="#">News Portal</a>
             </div>
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav">
-                    <li>
+                    <?php  
+                        $query = "SELECT * FROM categories";
+                        $get_all_catergories_query = mysqli_query($connection,$query);
+                        
+                        while($row = mysqli_fetch_assoc($get_all_catergories_query)){
+                            $cat_title = $row['cat_title'];
+                            echo "<li> <a href = '#'>{$cat_title}</a></li>";
+                        }
+                    ?>
+                    <!-- <li>
                         <a href="#">About</a>
                     </li>
-                    <li>
-                        <a href="#">Services</a>
-                    </li>
+                    
                     <li>
                         <a href="#">Contact</a>
-                    </li>
+                    </li> -->
                 </ul>
             </div>
             <!-- /.navbar-collapse -->
